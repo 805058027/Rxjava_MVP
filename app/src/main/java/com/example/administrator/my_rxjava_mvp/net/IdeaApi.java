2f7307d4a7dd89327ad6c42ff9d64f8fe75ecb49
805058027 <535382832@qq.com>
2018-03-15 15:41:21 +0800

    添加log

--- app/src/main/java/com/example/administrator/my_rxjava_mvp/net/IdeaApi.java
+++ app/src/main/java/com/example/administrator/my_rxjava_mvp/net/IdeaApi.java
@@ -1,9 +1,26 @@
 package com.example.administrator.my_rxjava_mvp.net;
 
+import com.example.administrator.my_rxjava_mvp.utils.LogUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import butterknife.internal.Utils;
+import okhttp3.Cache;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -16,9 +33,30 @@ public class IdeaApi {
     private IdeaApiService service;
 
     private IdeaApi() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                try {
+                    String text = URLDecoder.decode(message, "utf-8");
+                    LogUtils.e("OKHttp-----", text);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                    LogUtils.e("OKHttp-----", message);
+                }
+            }
+        });
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(Constants.DEFAULT_TIMEOUT, TimeUnit.MILLISECONDS)
+                .connectTimeout(Constants.DEFAULT_TIMEOUT, TimeUnit.MILLISECONDS)
+                .addInterceptor(interceptor)
+                .build();
+
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").serializeNulls().create();
 
         retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .baseUrl(Constants.API_SERVER_URL)
@@ -34,4 +72,5 @@ public class IdeaApi {
     public static IdeaApiService getApiService() {
         return SingletonHolder.INSTANCE.service;
     }
+
 }
